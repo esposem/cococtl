256fc4e0196f0cff4fcb3a3c1e602030a351c683
Pradipta Banerjee <pradipta.banerjee@gmail.com>
2025-11-12 18:07:39 +0530

    Fix KBS resource policy
    Use permissive as default
    
    Signed-off-by: Pradipta Banerjee <pradipta.banerjee@gmail.com>

--- pkg/trustee/trustee.go
+++ pkg/trustee/trustee.go
@@ -222,63 +222,10 @@ metadata:
   namespace: %s
 data:
   policy.rego: |
-    # Policy to allow specific KBS resource access and curl commands for attestation verification
+    package policy
+    import rego.v1
 
-    package agent_policy
-
-    import future.keywords.in
-    import future.keywords.if
-    import future.keywords.every
-
-    default AddARPNeighborsRequest := true
-    default AddSwapRequest := true
-    default CloseStdinRequest := true
-    default CopyFileRequest := true
-    default CreateSandboxRequest := true
-    default DestroySandboxRequest := true
-    default GetMetricsRequest := true
-    default GetOOMEventRequest := true
-    default GuestDetailsRequest := true
-    default ListInterfacesRequest := true
-    default ListRoutesRequest := true
-    default MemHotplugByProbeRequest := true
-    default OnlineCPUMemRequest := true
-    default PauseContainerRequest := true
-    default PullImageRequest := true
-    default ReadStreamRequest := true
-    default RemoveContainerRequest := true
-    default RemoveStaleVirtiofsShareMountsRequest := true
-    default ReseedRandomDevRequest := true
-    default ResumeContainerRequest := true
-    default SetGuestDateTimeRequest := true
-    default SignalProcessRequest := true
-    default StartContainerRequest := true
-    default StartTracingRequest := true
-    default StatsContainerRequest := true
-    default StopTracingRequest := true
-    default TtyWinResizeRequest := true
-    default UpdateContainerRequest := true
-    default UpdateEphemeralMountsRequest := true
-    default UpdateInterfaceRequest := true
-    default UpdateRoutesRequest := true
-    default WaitProcessRequest := true
-    default WriteStreamRequest := true
-    default CreateContainerRequest := true
-
-    default SetPolicyRequest := false
-    default ExecProcessRequest := false
-
-    ExecProcessRequest if {
-        input_command = concat(" ", input.process.Args)
-        some allowed_command in policy_data.allowed_commands
-        input_command == allowed_command
-    }
-
-    policy_data := {
-      "allowed_commands": [
-            "curl -s http://localhost:8006/cdh/resource/default/attestation-status/status"
-      ]
-    }
+    default allow = true
 ---
 apiVersion: v1
 kind: ConfigMap
